072c505a682e790a6c84cd1acd89fd6d86524c86
ajb-4 <alexbrown4711@gmail.com>
2023-08-15 16:49:32 -0400

    onto phase 2

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,5 +1,5 @@
 // Your code here
-Array.prototype.uniq = function() 
+Array.prototype.uniq = function() {
     let emptyArray = [];
 
     this.forEach( (el) => {
@@ -11,5 +11,35 @@ Array.prototype.uniq = function()
     //console.log(emptyArray)
     return emptyArray 
 };
+
+Array.prototype.twoSum = function() {
+    let emptyArray = [];
+
+    this.forEach((el1, idx1) => {
+        this.forEach((el2, idx2) => {
+            if(idx2>idx1 && el1 + el2 === 0) {
+                emptyArray.push([idx1, idx2])
+            } 
+        })
+    })
+    return emptyArray
+};
+
+Array.prototype.transpose = function() {
+    const rowlength = this.length;
+    const collength = this[0].length;
+
+    let emptyArray = [];
+        for (let i = 0; i < collength; i++) {
+    let newArray = [];
+            emptyArray.push(newArray);
+        for (let j = 0; j < rowlength; j++) {
+               emptyArray[i][j] = this[j][i];
+        }
+    }
+    return emptyArray
+};
+
+
 
 
