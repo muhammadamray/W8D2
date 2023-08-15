728ebcf6986c4d89a45761f6cf704cc2085695af
Muhammad Amray <muhammadamray@gmail.com>
2023-08-15 15:07:24 -0400

    phase 1

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,15 @@
 // Your code here
+Array.prototype.uniq = function() 
+    let emptyArray = [];
+
+    this.forEach( (el) => {
+        if (!emptyArray.includes(el)) {
+            emptyArray.push(el)
+
+        }
+    })
+    //console.log(emptyArray)
+    return emptyArray 
+};
+
+
