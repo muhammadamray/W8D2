818d57cada83f0ea8bfc3bf1b0e49f42a92f84bd
ajb-4 <alexbrown4711@gmail.com>
2023-08-15 17:26:29 -0400

    phase 2 myMap is next

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,12 @@
 // Your code here
+
+Array.prototype.myEach = function(banana) {
+    for (let i = 0; i < this.length; i++) {
+        banana(this[i])
+    }
+};
+function bracketer(el) {
+    console.log([el]);
+}
+[1,2].myEach(bracketer)
+
